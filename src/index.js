598e9959e86198889837d388a366938cadfaf724
Anna <kazanok777@gmail.com>
2022-07-01 09:34:23 +0300

    fixed prettier settings

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 export const hotel = {
-    quantityOfPlaces: 30,
-    priceByPlace: 20,
-    bankAccount: 0,
-    guests: {},
-    getLength: function() {
-        return Object.keys(this.guests).length;
-    },
-    getActualFreePlace: function() {
-        return this.quantityOfPlaces - this.getLength();
-    },
-    paidPerPlace: function() {
-        this.bankAccount += this.priceByPlace;
-    },
-    checkInGuest: function(firstName, lastName, money) {
-        if (this.getActualFreePlace() === 0) {
-            return 'Sorry, we have not free spaces';
-        } else if (money < this.priceByPlace) {
-            return 'Sorry, you have not enough money';
-        }
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+  getActualFreePlace: function () {
+    return this.quantityOfPlaces - this.getLength();
+  },
+  paidPerPlace: function () {
+    this.bankAccount += this.priceByPlace;
+  },
+  checkInGuest: function (firstName, lastName, money) {
+    if (this.getActualFreePlace() === 0) {
+      return 'Sorry, we have not free spaces';
+    } else if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    }
 
-        function Guest(firstName, lastName, money) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.money = money;
-        }
-        this.paidPerPlace();
-        return (this.guests[this.getLength()] = new Guest(
-            firstName,
-            lastName,
-            money - this.priceByPlace
-        ));
-    },
+    function Guest(firstName, lastName, money) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.money = money;
+    }
+    this.paidPerPlace();
+    return (this.guests[this.getLength()] = new Guest(
+      firstName,
+      lastName,
+      money - this.priceByPlace
+    ));
+  },
 };
